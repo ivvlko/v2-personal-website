a9d17db23687ff7c6631cb24da1619b018bfe2e8
ivvlko <ivankoevbg@gmail.com>
2025-03-20 17:43:11 +0200

    fix media files

--- personal_website/personal_website/urls.py
+++ personal_website/personal_website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -12,3 +14,5 @@ urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
